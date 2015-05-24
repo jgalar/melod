2bc0aefbc9336e9f7fd4fcecc2379051788fd59a
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2015-05-24 10:57:57 -0400

    server: Stub implementation of Artists API

---
+++ server/melod.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask.ext import restful
+from flask_restful import reqparse
+# Only used for HTTP status codes
+import http.client as http
+
+app = Flask("melod")
+api = restful.Api(app)
+
+
+class Artists(restful.Resource):
+    def __init__(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('sort', type=str)
+        parser.add_argument('artists_per_page', type=int)
+        parser.add_argument('page', type=int)
+        parser.add_argument('starts_with', type=str)
+        self.parser = parser
+
+    def get(self):
+        args = self.parser.parse_args()
+        print(args)
+        # Return a list of available artists
+        return {'artists': str(range(42))}
+
+
+api.add_resource(Artists, '/artists')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
